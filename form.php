d7cc1f24d09a77faa77ad5d645f45177d041e65f
Smit panchal <smitpanchal235@gmail.com>
2019-08-19 21:57:47 -0400

    form added

---
+++ form.php
@@ -0,0 +1,26 @@
+<?php
+require('fpdf.php');
+
+$pdf=new FPDF();
+$pdf->AddPage();
+$pdf->SetFont('Arial','B',16);
+$pdf->Cell(40,10,'Hello World!');
+$pdf->Output();
+?>
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
