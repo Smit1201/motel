ea350ff499f09102203c08e060edaa0025d65ce3
Smit panchal <smitpanchal235@gmail.com>
2019-08-19 22:21:03 -0400

    reception gallary file added

---
+++ gallery_RES.php
@@ -0,0 +1,310 @@
+<!--
+author: W3layouts
+author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Super8 Cortez | Gallery_RECEPTIONIST</title>
+        <!-- custom-theme -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="keywords" content="Super8 Cortez ,505 East Main Street, Cortez, CO 81321, USA" />
+        <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+            function hideURLbar(){ window.scrollTo(0,1); } </script>
+        <!-- //custom-theme -->
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+        <link rel="stylesheet" href="css/lightbox.css">
+        <!-- js -->
+        <script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+        <script src="js/main.js"></script>
+        <!-- //js -->
+        <!-- font-awesome-icons -->
+        <link href="css/font-awesome.css" rel="stylesheet"> 
+        <!-- tabs -->
+        <link href="css/easy-responsive-tabs.css" rel='stylesheet' type='text/css'/>
+        <!-- //tabs -->
+        <!-- //font-awesome-icons -->
+        <link href="//fonts.googleapis.com/css?family=Prompt:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=latin-ext,thai,vietnamese" rel="stylesheet">
+        <link href="//fonts.googleapis.com/css?family=Merriweather" rel="stylesheet">
+    </head>
+    <body>
+        <!-- banner -->
+        <div class="banner-1">
+            <div class="header-bottom">
+                <div class="header">
+                    <div class="container">
+                        <div class="w3_agile_logo">
+                            <h1><a href="HOME_OWNER.php"><span style="color: yellow">Super</span>Eight</a></h1>
+                        </div>
+                        <div class="header-nav">
+                            <nav class="navbar navbar-default">
+                                <div class="navbar-header navbar-left">
+                                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                                        <span class="sr-only">Toggle navigation</span>
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                    </button>
+                                </div>
+                                <!-- Collect the nav links, forms, and other content for toggling -->
+                                <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+                                    <nav class="link-effect-12">
+                                        <ul class="nav navbar-nav w3_agile_nav">
+                                            <li class="active">
+                                                <a href="HOME_RES.php"><span>Home</span></a></li>
+                                            <li><a href="gallery_RES.php"><span>Gallery</span></a></li>
+                                            <li><a href="REGISTRATION_EMP.php"><span>Enroll Staff</span></a></li>
+                                            <li class="dropdown">
+                                                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span data-hover="Short Codes">Check...</span> <b class="caret"></b></a>
+                                                <ul class="dropdown-menu agile_short_dropdown">
+                                                    <li><a href="checkIN.php">Check-In</a></li>
+                                                    <li><a href="checkOUT.php">Check-Out</a></li>
+                                                </ul>
+                                            </li>
+
+                                            <li><a href="Book_Room.php">Book Room</a></li>
+
+                                            <li><a href="contact_RES.php"><span>Contact</span></a></li>
+                                            <li><a href="about_RES.php"><span>About Us</span></a></li>
+                                            <li><a href="LOGIN.php"> LOGOUT </a></li>
+
+                                        </ul>	
+                                    </nav>
+                                </div>
+                            </nav>
+                        </div>
+                        <div class="clearfix"> </div>
+                    </div>
+                </div>
+                <!-- header -->
+            </div>
+        </div>
+        <!-- //banner -->
+        <!-- gallery -->
+        <div class="gallery">
+            <div class="container">
+                <div class="w3ls-heading">
+                    <h2>Gallery</h2>
+                </div>
+                <div class="gallery-grids">
+                    <div class="col-md-4 gallery-grid">
+                        <div class="grid">
+                            <figure class="effect-apollo">
+                                <a class="example-image-link" href="images/4.jpg" data-lightbox="example-set" >
+                                    <img src="images/4.jpg" alt="" style="width:100%; height:100%"/>
+                                    <figcaption>
+                                        
+                                    </figcaption>	
+                                </a>
+                            </figure>
+                        </div>
+                    </div>
+                    <div class="col-md-4 gallery-grid">
+                        <div class="grid">
+                            <figure class="effect-apollo">
+                                <a class="example-image-link" href="images/101.jpg" data-lightbox="example-set" >
+                                    <img src="images/101.jpg" alt="" style="width:100%; height:235px"/>
+                                    <figcaption>
+                                        
+                                    </figcaption>	
+                                </a>
+                            </figure>
+                        </div>
+                    </div>
+                    <div class="col-md-4 gallery-grid">
+                        <div class="grid">
+                            <figure class="effect-apollo">
+                                <a class="example-image-link" href="images/92913288(4).jpg" data-lightbox="example-set" >
+                                    <img src="images/92913288(4).jpg" alt="" style="width:100%; height:235px"/>
+                                    <figcaption>
+                                    </figcaption>		
+                                </a>
+                            </figure>
+                        </div>
+                    </div>
+                    <div class="col-md-4 gallery-grid">
+                        <div class="grid">
+                            <figure class="effect-apollo">
+                                <a class="example-image-link" href="images/5950849(17).jpg" data-lightbox="example-set" >
+                                    <img src="images/5950849(17).jpg" alt="" style="width:100%; height:235px"/>
+                                    <figcaption>
+                                       
+                                    </figcaption>	
+                                </a>
+                            </figure>
+                        </div>
+                    </div>
+                    <div class="col-md-4 gallery-grid">
+                        <div class="grid">
+                            <figure class="effect-apollo">
+                                <a class="example-image-link" href="images/92913300(8).jpg" data-lightbox="example-set" >
+                                    <img src="images/92913300(8).jpg" alt="" style="width:100%; height:235px" />
+                                    <figcaption>
+                                    </figcaption>	
+                                </a>
+                            </figure>
+                        </div>
+                    </div>
+                    <div class="col-md-4 gallery-grid">
+                        <div class="grid">
+                            <figure class="effect-apollo">
+                                <a class="example-image-link" href="images/res.jpg" data-lightbox="example-set" >
+                                    <img src="images/res.jpg" alt="" style="width:100%; height:235px" />
+                                    <figcaption>
+                                    </figcaption>		
+                                </a>
+                            </figure>
+                        </div>
+                    </div>
+                    <div class="col-md-4 gallery-grid">
+                        <div class="grid">
+                            <figure class="effect-apollo">
+                                <a class="example-image-link" href="images/xyz.jpg" data-lightbox="example-set" >
+                                    <img src="images/xyz.jpg" alt="" style="width:100%; height:235px"/>
+                                    <figcaption>
+                                    </figcaption>	
+                                </a>
+                            </figure>
+                        </div>
+                    </div>
+                    <div class="col-md-4 gallery-grid">
+                        <div class="grid">
+                            <figure class="effect-apollo">
+                                <a class="example-image-link" href="images/road.jpg" data-lightbox="example-set" >
+                                    <img src="images/road.jpg" alt="" style="width:100%; height:235px"/>
+                                    <figcaption>
+                                        
+                                    </figcaption>	
+                                </a>
+                            </figure>
+                        </div>
+                    </div>
+                    <div class="col-md-4 gallery-grid">
+                        <div class="grid">
+                            <figure class="effect-apollo">
+                                <a class="example-image-link" href="images/kichen.jpg" data-lightbox="example-set" >
+                                    <img src="images/kichen.jpg" alt="" style="width:100%; height:235px"/>
+                                    <figcaption>
+                                    </figcaption>		
+                                </a>
+                            </figure>
+                        </div>
+                    </div>
+                    <div class="clearfix"> </div>
+                    <script src="js/lightbox-plus-jquery.min.js"></script>
+                </div>
+            </div>
+        </div>
+        <!-- //gallery -->
+        <!-- footer -->
+        <div class="w3-agile-footer">
+            <div class="container">
+                <div class="footer-grids">
+                    <div class="col-md-3 footer-grid">
+                        <div class="footer-grid-heading">
+                            <h4>Address</h4>
+                        </div>
+                        <div class="footer-grid-info">
+                            <p>505 East Main Street,
+                                <span>Cortez, CO 81321, USA</span>
+                            </p>
+                            <p class="phone">Phone :( +1 123 456 789)
+                                <span>Email : <a href="mailto:example@email.com">mail@example.com</a></span>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-3 footer-grid">
+                        <div class="footer-grid-heading">
+                            <h4>Navigation</h4>
+                        </div>
+                        <div class="footer-grid-info">
+                            <ul>
+                                <li><a href="HOME_RES.php">Home</a></li>
+                                <li><a href="about_RES.php">About</a></li>
+                                <li><a href="gallery_RES.php">Gallery</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <!-- //footer -->
+            <!-- start-smooth-scrolling -->
+            <script type="text/javascript" src="js/move-top.js"></script>
+            <script type="text/javascript" src="js/easing.js"></script>
+            <script type="text/javascript">
+                jQuery(document).ready(function ($) {
+                    $(".scroll").click(function (event) {
+                        event.preventDefault();
+                        $('html,body').animate({scrollTop: $(this.hash).offset().top}, 1000);
+                    });
+                });
+            </script>
+            <!-- start-smooth-scrolling -->
+            <!-- for bootstrap working -->
+            <script src="js/bootstrap.js"></script>
+            <!-- //for bootstrap working -->
+            <!-- here stars scrolling icon -->
+            <script type="text/javascript">
+                $(document).ready(function () {
+                    /*
+                     var defaults = {
+                     containerID: 'toTop', // fading element id
+                     containerHoverID: 'toTopHover', // fading element hover id
+                     scrollSpeed: 1200,
+                     easingType: 'linear' 
+                     };
+                     */
+
+                    $().UItoTop({easingType: 'easeOutQuart'});
+
+                });
+            </script>
+            <!-- //here ends scrolling icon -->
+            <script src='js/jquery.typer.js'></script>
+            <script>
+                var win = $(window),
+                        foo = $('#typer');
+
+                foo.typer(['Luxury Hotels', 'Hotels & Resorts', 'Luxury Resorts']);
+
+        // unneeded...
+                win.resize(function () {
+                    var fontSize = Math.max(Math.min(win.width() / (1 * 10), parseFloat(Number.POSITIVE_INFINITY)), parseFloat(Number.NEGATIVE_INFINITY));
+
+                    foo.css({
+                        fontSize: fontSize * .8 + 'px'
+                    });
+                }).resize();
+            </script><!--tabs-->
+            <script src="js/easy-responsive-tabs.js"></script>
+            <script>
+                $(document).ready(function () {
+                    $('#horizontalTab').easyResponsiveTabs({
+                        type: 'default', //Types: default, vertical, accordion           
+                        width: 'auto', //auto or any width like 600px
+                        fit: true, // 100% fit in a container
+                        closed: 'accordion', // Start closed if in accordion view
+                        activate: function (event) { // Callback function if tab is switched
+                            var $tab = $(this);
+                            var $info = $('#tabInfo');
+                            var $name = $('span', $info);
+                            $name.text($tab.text());
+                            $info.show();
+                        }
+                    });
+                    $('#verticalTab').easyResponsiveTabs({
+                        type: 'vertical',
+                        width: 'auto',
+                        fit: true
+                    });
+                });
+            </script>
+            <!--//tabs-->
+    </body>
+</html>
+
